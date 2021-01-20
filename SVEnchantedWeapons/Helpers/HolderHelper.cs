9ceaa35988cfe6f46c47629d1c3a0c06e6203d9d
Joe van de Bilt <joevandebilt@live.co.uk>
2021-01-20 09:36:50 +0000

    Initial Commit of starter files

---
+++ SVEnchantedWeapons/Helpers/HolderHelper.cs
@@ -0,0 +1,43 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVEnchantedWeapons.Helpers
+{
+    public static class HolderHelper
+    {
+        private static Dictionary<SButton, DateTime> keyPresses = new Dictionary<SButton, DateTime>();
+
+        public static void SetReleaseWindow(SButton button, double releaseWindowMs, IMonitor monitor)
+        {
+            var releaseTime = DateTime.Now.AddMilliseconds(releaseWindowMs);
+            if (keyPresses.ContainsKey(button))
+            {
+                monitor.Log($"Button {button} is already in dictionary - adding it with a release time of {releaseTime}");
+                keyPresses[button] = releaseTime;
+            }
+            else
+            {
+                monitor.Log($"Button {button} is not in dictionary - adding it with a release time of {releaseTime}");
+                keyPresses.Add(button, releaseTime);
+            }
+        }
+
+        public static bool CheckReleasedWindow(SButton button, IMonitor monitor)
+        {
+            if (keyPresses.ContainsKey(button))
+            {
+                var now = DateTime.Now;
+                var resolved = (keyPresses[button] <= now);
+                monitor.Log($"Button {button} is in dictionary target is {keyPresses[button]} and it is currently {now} - Charge Resolved: {resolved}");
+
+                return resolved;
+            }
+            return false;
+        }
+    }
+}
---
+++ SVEnchantedWeapons/SVEWMod.cs
@@ -0,0 +1,53 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI.Events;
+using SVEnchantedWeapons.Helpers;
+using StardewValley;
+using StardewValley.Tools;
+
+namespace SVEnchantedWeapons
+{
+    public class SVEWMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            //Add the hold button event
+            helper.Events.Input.ButtonPressed += Input_ButtonPressed;
+
+            //Add the button released event
+            helper.Events.Input.ButtonReleased += Input_ButtonReleased;
+        }
+
+        private void Input_ButtonReleased(object sender, ButtonReleasedEventArgs e)
+        {
+            if (Context.IsPlayerFree)
+            {
+                this.Monitor.Log($"Player released {e.Button}.", LogLevel.Debug);
+                if (HolderHelper.CheckReleasedWindow(e.Button, this.Monitor)) {
+                    //Fire enchantment
+                    this.Monitor.Log($"Player held the button for the required amount of time", LogLevel.Debug);
+                }
+            }
+        }
+
+        private void Input_ButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (Context.IsPlayerFree)
+            {
+                this.Monitor.Log($"Player pressed {e.Button}.", LogLevel.Debug);
+                if (e.Button == SButton.MouseLeft)
+                {
+                    if (Game1.player.CurrentItem as MeleeWeapon != null)
+                    {
+                        this.Monitor.Log($"Player is charging a weapon - {Game1.player.CurrentItem.Name} - isCharging: {Game1.player.isCharging}", LogLevel.Debug);
+                        HolderHelper.SetReleaseWindow(e.Button, 1000, this.Monitor);
+                    }
+                }
+            }
+        }
+    }
+}
